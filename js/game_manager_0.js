aea8d8229db3da90cb0563a8658f4e7e00a3ca5b
Mean Triag <triangleweakapuz@gmail.com>
2016-12-06 17:29:35 +0000

    Update game_manager_0.js

--- js/game_manager_0.js
+++ js/game_manager_0.js
@@ -72,7 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 0 : 0;
+    var value = Math.random() < 0.9 ? 7 : 14;
     var tile = new Tile(this.grid.randomAvailableCell(), 0);
 
     this.grid.insertTile(tile);
@@ -158,7 +158,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, 0);
+          var merged = new Tile(positions.next * 7);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -171,7 +171,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === 2401) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
